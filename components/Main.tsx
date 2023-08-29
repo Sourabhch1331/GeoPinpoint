06a7bd94a5e086b98c580b578d237747bcea23bb
Sourabh Choudhary <Sourabhch1331@gmail.com>
2023-08-29 11:07:09 +0530

    implemented proper error handling

--- components/Main.tsx
+++ components/Main.tsx
@@ -11,43 +11,45 @@ interface Location {
 }
 
 const Main:React.FC = ()  => {
-    const [loading,setLoading] =  useState<Boolean>(false);
+    const [loading,setLoading] =  useState<boolean>(false);
     const [locationInfo,setLocationINfo] = useState<Location | null>(null);
+    const [err,setErr] = useState<any>(null);
+    
 
-
-    // TO get the IP address of user
-    const getMyIpAddress = async (url:string) :Promise<string> => {
-        const res = await fetch(url);
-        const info = await res.json();
-        return info.ip;
-    }
-
-    // To get location using IP address
-    const getMyLocation= async (url:string) :Promise<Location> => {
-        const res = await fetch(url);
-        const info = await res.json();
-        
-        const country:string = new Intl.DisplayNames(['en'], {type: 'region'}).of(info.country); 
-
-        return {
-            ip: info.ip,
-            city:info.city,
-            country,
-            region: info.region
-        }
-    }
+//    console.log(err);
 
     // click handler 
     const getUserLocation = async()=> {
         setLoading(prev => true);
-
         const accessTokeForIpinfo:string | undefined = process.env.PLASMO_PUBLIC_IPINFO_ACCESS_TOKEN;
 
-        const myIpAddress: string = await getMyIpAddress(`https://api64.ipify.org?format=json`);
-        const currLocation:Location = await getMyLocation(`https://ipinfo.io/${myIpAddress}?token=${accessTokeForIpinfo}`);
+        try{
+            // get the ip address
+            const resIp = await fetch(`https://api64.ipify.org?format=json`);
+            const info = await resIp.json();
+            const myIpAddress=info.ip;
 
-        setLocationINfo(prev => currLocation);
-        setLoading(prev => false);
+            // get the location based on ip address
+            const resLoc = await fetch(`https://ipinfo.io/${myIpAddress}?token=${accessTokeForIpinfo}`);
+            const infoLoc = await resLoc.json();
+            
+            // get the country from country code
+            const country:string = new Intl.DisplayNames(['en'], {type: 'region'}).of(infoLoc.country); 
+
+            // set the curr state
+            setLocationINfo({
+                ip: infoLoc.ip,
+                city:infoLoc.city,
+                country,
+                region: infoLoc.region
+            });
+        }
+        catch(err:any){
+            setErr(prev => err);
+        }
+        finally{
+            setLoading(prev => false);
+        }
     }
 
 
@@ -55,17 +57,23 @@ const Main:React.FC = ()  => {
 
         <main className="h-[80vh] flex flex-col items-center justify-evenly">
             
-            {
-                locationInfo!=null
-                    && 
-                <h2 className="text-xl font-semibold">
-                    Your country is {locationInfo.country} and city is {locationInfo.city}.
-                </h2>
+            {   
+                err ?
+                    <h2 className="text-xl font-semibold">
+                        {err.message ? err.message:"something went wrong!"}
+                    </h2>
+                :
+                    locationInfo
+                        && 
+                    <h2 className="text-xl font-semibold">
+                        Your country is {locationInfo.country} and city is {locationInfo.city}.
+                    </h2>
             }
 
             <button 
                 className="bg-red-600 text-xl text-white font-bold p-3 rounded-lg duration-200 hover:scale-110"
                 onClick={getUserLocation}
+                disabled={loading}
             > 
                 {loading ? <Loading/>:`show my location`}
             </button>
