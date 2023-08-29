920660aefa29fec08392fe2791139e60c7968f90
Sourabh Choudhary <Sourabhch1331@gmail.com>
2023-08-29 11:10:39 +0530

    changed font size

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,9 +3,9 @@ import location from "data-base64:~assets/location.png"
 const Header:React.FC = ()=> {
     return (
         <header className="h-[20vh] flex items-center">
-            <h1 className="m-auto text-2xl font-semibold flex items-center justify-center">
+            <h1 className="m-auto text-3xl font-semibold flex items-center justify-center">
 			    Geo Pinpoint
-			    <img className="h-8 w-8 pointer-events-none" src={location} alt="Some pretty cool image" />
+			    <img className="h-10 w-10 pointer-events-none" src={location} alt="Some pretty cool image" />
 			</h1>
         </header>
     )
